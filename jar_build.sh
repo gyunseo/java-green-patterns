382cd01811b9ea958e3495a525d15b36fe4b4dbd
Gyunseo Lee <rbstj0311@naver.com>
2023-11-26 00:59:55 +0900

    음하하 완성

--- jar_build.sh
+++ jar_build.sh
@@ -1,6 +1,8 @@
 chmod u+x ./**/**/*.sh
 echo "delete all jar file"
 rm -rf ./**/**/*.jar
+echo "delete all zip file"
+rm -rf *.zip
 
 echo "Start build jar files"
 
@@ -16,9 +18,9 @@ mv After.jar after_1.jar
 
 echo "start build jar file for efficient-data-structure"
 cd ../../efficient-data-structure/Before/
+./before.sh
 echo "rename jar file to before green pattern #2"
 mv Before.jar before_2.jar
-./before.sh
 cd ../After/
 ./after.sh
 echo "rename jar file to after green pattern #2"
@@ -33,3 +35,17 @@ cd ../After/
 ./after.sh
 echo "rename jar file to after green pattern #3"
 mv After.jar after_3.jar
+
+cd ../../
+echo "zip all jar files"
+
+# 최종적으로 생성할 zip 파일
+zipfile="jar_files.zip"
+
+# .jar 파일을 찾아서 압축
+find . -name '*.jar' | while read file; do
+    dir=$(dirname "$file")
+    filename=$(basename "$file")
+    (cd "$dir" && zip -r "$OLDPWD/$zipfile" "$filename")
+done
+
