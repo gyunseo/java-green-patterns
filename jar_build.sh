ea89ff05cf7655f586b07afdaf8c114755b09921
Gyunseo Lee <rbstj0311@naver.com>
2023-11-26 00:46:42 +0900

    완성

--- jar_build.sh
+++ jar_build.sh
@@ -1,21 +1,35 @@
 chmod u+x ./**/**/*.sh
+echo "delete all jar file"
+rm -rf ./**/**/*.jar
 
-echo "Start build jar file"
+echo "Start build jar files"
 
 echo "start build jar file for lazy-initialize"
 cd lazy-initialize/Before/
 ./before.sh
+echo "rename jar file to before green pattern #1"
+mv Before.jar before_1.jar
 cd ../After/
 ./after.sh
+echo "rename jar file to after green pattern #1"
+mv After.jar after_1.jar
 
 echo "start build jar file for efficient-data-structure"
 cd ../../efficient-data-structure/Before/
+echo "rename jar file to before green pattern #2"
+mv Before.jar before_2.jar
 ./before.sh
 cd ../After/
 ./after.sh
+echo "rename jar file to after green pattern #2"
+mv After.jar after_2.jar
 
 echo "start build jar file for object-pool"
 cd ../../object-pool/Before/
 ./before.sh
+echo "rename jar file to before green pattern #3"
+mv Before.jar before_3.jar
 cd ../After/
 ./after.sh
+echo "rename jar file to after green pattern #3"
+mv After.jar after_3.jar
