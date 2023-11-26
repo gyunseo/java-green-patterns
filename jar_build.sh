d78b51bcd9f3d042017af25559b258d4c22e8d93
Gyunseo Lee <rbstj0311@naver.com>
2023-11-26 22:02:53 +0900

    최종

--- jar_build.sh
+++ jar_build.sh
@@ -7,14 +7,15 @@ rm -rf *.zip
 echo "Start build jar files"
 
 echo "start build jar file for lazy-initialize"
-cd lazy-initialize/Before/
+cd ./lazy-initialize/Before/
 ./before.sh
-echo "rename jar file to before green pattern #1"
-mv Before.jar before_1.jar
+echo "rename jar file to before green pattern #19"
+mv Before.jar before_19.jar
 cd ../After/
 ./after.sh
-echo "rename jar file to after green pattern #1"
-mv After.jar after_1.jar
+echo "rename jar file to after green pattern #19"
+mv After.jar after_19.jar
+
 
 echo "start build jar file for efficient-data-structure"
 cd ../../efficient-data-structure/Before/
@@ -29,16 +30,30 @@ mv After.jar after_2.jar
 echo "start build jar file for object-pool"
 cd ../../object-pool/Before/
 ./before.sh
-echo "rename jar file to before green pattern #3"
-mv Before.jar before_3.jar
+echo "rename jar file to before green pattern #17"
+mv Before.jar before_17.jar
 cd ../After/
 ./after.sh
-echo "rename jar file to after green pattern #3"
-mv After.jar after_3.jar
+echo "rename jar file to after green pattern #17"
+mv After.jar after_17.jar
 
+echo "test all jar files work well"
 cd ../../
-echo "zip all jar files"
+/usr/bin/time -v echo "test before green pattern #19"
+time java -jar ./lazy-initialize/Before/before_19.jar
+echo "test after green pattern #19"
+/usr/bin/time -v java -jar ./lazy-initialize/After/after_19.jar
+echo "test before green pattern #2"
+/usr/bin/time -v java -jar ./efficient-data-structure/Before/before_2.jar
+echo "test after green pattern #2"
+/usr/bin/time -v java -jar ./efficient-data-structure/After/after_2.jar
+echo "test before green pattern #17"
+/usr/bin/time -v java -jar ./object-pool/Before/before_17.jar
+echo "test after green pattern #17"
+/usr/bin/time -v java -jar ./object-pool/After/after_17.jar
 
+
+echo "zip all jar files"
 # 최종적으로 생성할 zip 파일
 zipfile="jar_files.zip"
 
